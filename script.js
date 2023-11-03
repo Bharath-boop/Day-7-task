6c5e836eff34bbec56676b287df3204a044e2ea8
Bharath-boop <bharathboopathi51550@gmail.com>
2023-11-03 20:21:03 +0530

    latest update(03/11/2023)

--- script.js
+++ script.js
@@ -4,10 +4,11 @@ result.send();
 result.onload=function(){
     const response=result.response;
     var responsedata=JSON.parse(response);
- //   console.log(responsedata);
-    console.log(responsedata.filter((contry)=>contry.region==="Asia"));
+   // console.log(responsedata);
+    const name=responsedata.filter((contry)=>contry.region==="Asia");
+    name.forEach((cn1)=>console.log(cn1.name.common));
     const population1=responsedata.filter((pop)=>pop.population<200000);
-    population1.map((cn)=> console.log(cn.name.common));
+    population1.forEach((cn2)=>console.log(cn2.name.common));
     responsedata.forEach(element => {console.log(`
       Name:${element.name.common}
    capital:${element.capital} 
@@ -17,5 +18,6 @@ result.onload=function(){
         return a+r
     },0)
      console.log("Total population:" +total);
- 
+   const curr=responsedata.filter((cur)=>cur.currencies && cur.currencies.USD)
+    curr.forEach((usd)=>console.log(usd.name.common))
 }
